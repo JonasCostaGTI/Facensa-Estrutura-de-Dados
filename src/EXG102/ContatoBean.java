6f1c322c265f2844da6aca8b4910b874f503ed0e
Jonas <jonas.costa@icloud.com>
2016-03-08 15:57:32 -0300

    criacao dos metodos de exclusao e procura

--- src/EXG102/ContatoBean.java
+++ src/EXG102/ContatoBean.java
@@ -1,60 +1,85 @@
 package EXG102;
 
 public class ContatoBean {
-	
-		
-		Contato[] contatos = new Contato[4];
-		
-		
-		
-		
-	
-	
-	
-	
-	
-	
-	public void inserirContato(Contato ...contato) {
+
+	Contato[] contatos = new Contato[4];
+
+	public void inserirContato(Contato... contato) {
 		for (int i = 0; i < contatos.length; i++) {
 			if (contatos[i] == null) {
 				contatos[i] = contato[i];
-				
+
 				i = contatos.length;
 			}
-			
-	      }
-		
 		}
-	
+	}
+
 	public Contato pegaContatoPorNome(String nome) {
-		
+		Contato contato = null;
 		for (int i = 0; i < contatos.length; i++) {
-			if (contatos[i].equals(nome)) {
-				Contato contato = contatos[i];
-				return contato;
+			if (contatos[i].getNome().equals(nome)) {
+				contato = contatos[i];
 			}
 		}
-		return null;
-		
+		if (contato == null) {
+			System.out.println("contato com o Nome: " + nome + ", nao encontrado");
+		}
+		return contato;
 	}
-	
+
 	public void imprimir() {
 		for (int i = 0; i < contatos.length; i++) {
-			
 			if (contatos[i] != null) {
-				System.out.println("contato " + (i+1));
+				System.out.println("contato: " + i);
 				System.out.println("Nome: " + contatos[i].getNome());
 				System.out.println("Email: " + contatos[i].getEmail());
 				System.out.println("Telefone: " + contatos[i].getTelefone());
 			}
-			
 			System.out.println("");
 		}
 	}
-	
-	
-	
-	
-	
-}
 
+	public Contato pegaContatoPorEmail(String email) {
+		Contato contato = null;
+		for (int i = 0; i < contatos.length; i++) {
+			if (contatos[i].getEmail().equals(email)) {
+				contato = contatos[i];
+			}
+		}
+		if (contato == null) {
+			System.out.println("contato com o Email: " + email + ", nao encontrado");
+		}
+		return contato;
+	}
+
+	public Contato pegaContatoPorTelefone(String telefone) {
+		Contato contato = null;
+		for (int i = 0; i < contatos.length; i++) {
+			if (contatos[i].getTelefone().equals(telefone)) {
+				contato = contatos[i];
+			}
+		}
+		if (contato == null) {
+			System.out.println("contato com o Telefone: " + telefone + ", nao encontrado");
+		}
+		return contato;
+	}
+
+	public void removeContatoPorNome(String nome) {
+		int conta = 0;
+		for (int i = 0; i < contatos.length; i++) {
+			if (contatos[i].getNome().equals(nome)) {
+				contatos[i] = null;
+				conta++;
+			}
+		}
+
+		if (conta != 0) {
+			System.out.println("O contato " + nome + ", foi excluido com sucesso!");
+		}else {
+			System.out.println("Contado " + nome + " nao encontrado para ser excluido");
+		}
+
+	}
+
+}
